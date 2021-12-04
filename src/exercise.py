dfc4658e29c438a745b36d31086f6ba258df6d07
Justin Turner <mrfathed@protonmail.com>
2021-12-04 12:03:02 -0600

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #write your code below this line
+    age = int(input("How old are you?"))
+
+    if age >= 0 and age <= 120:
+        print("OK")
+    else:
+        print("Impossible!")
 
 if __name__ == '__main__':
     main()
